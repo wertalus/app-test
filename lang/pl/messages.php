0d04c261a91bc117db43e5c65e3af9f243794adb
Daniel Pater <info@danweb.pl>
2025-05-18 17:25:57 +0200

    First commit

---
+++ lang/pl/messages.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'Home' => 'Strona Domowa',
+    'Name' => 'Imię',
+    'Surname' => 'Nazwisko',
+    'Email Address' => 'Adres Email',
+    'Password' => 'Hasło',
+    'Confirm Password' => 'Potwierdź Hasło',
+    'Register' => 'Rejestracja',
+    'Login' => 'Zaloguj',
+    'login' => 'Zaloguj',
+
+    'failed' => 'Podane dane są niepoprawne.',
+    'password' => 'Hasło jest niepoprawne.',
+    'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds seconds.',
+    'reset' => 'Twoje hasło zostało zresetowane.',
+    'sent' => 'Wysłaliśmy lind do zresetowania hasła na Twój adres email.',
+    'throttled' => 'Zaczekaj zanim spróbujesz ponownie.',
+    'token' => 'Token do zresetowania hasła jest niepoprawny.',
+    'user' => "Nie mozemy znaleźć uytkownika o podanym adresie email.",
+    'You are logged in!'=>'Zostałeś zalogowany/a.',
+    'Remember Me' => 'Zapamiętaj mnie.',
+    'Forgot Your Password?' => 'Zapomiałeś/aś hasła ?',
+    'Verify Your Email Address' => 'Zweryfikuj adres mailowy.',
+    'A fresh verification link has been sent to your email address' => 'Nowy link weryfikacyjny został wysłany na adres email.',
+    'Before proceeding, please check your email for a verification link.' => 'Przed kontynuacją, sprawdź czy otrzymałeś link weryfikacyjny na swoją skrzynkę email ',
+    'If you did not receive the email' => 'Jeśli nie otrzymałeś maila',
+    'click here to request another' => 'kilknij tu, aby otrzymać kolejny',
+    'Please confirm your password before continuing.' => 'Proszę potwierdź hasło przed kontynuacją',
+    'Reset Password' => 'Zresetuj Hasło',
+    'Send Password Reset Link' => 'Wyślij link do zresetowania hasła',
+    ''
+
+];
